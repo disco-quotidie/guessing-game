6ab2866789889c686410393a61a7b2d90136f201
disco-quotidie <ccprasong@outlook.com>
2024-01-06 02:34:08 +0700

    rand Cargo functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::io;
+use rand::Rng;
 // the above are called prelude
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Secret number is : {secret_number}");
 
     println!("Please input your guess!");
 
