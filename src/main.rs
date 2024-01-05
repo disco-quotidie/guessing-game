eafd5801d75b1ccf53d4b45a87973adde97d7075
disco-quotidie <ccprasong@outlook.com>
2024-01-06 02:17:40 +0700

    basic IO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+// the above are called prelude
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess!");
+
+    // Rust !== JavaScript
+    // let a === const a 
+    // let mut a === let a
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}")
 }
